a8f172ff41c852d39894e9259a24789ec29770d1
Stacy Grimes <140400457+stacygrimes@users.noreply.github.com>
2024-03-19 15:26:03 -0400

    Update downloadEventLogsToCsv.sh

--- API Scripts/Bitwarden Public API/downloadEventLogsToCsv.sh
+++ API Scripts/Bitwarden Public API/downloadEventLogsToCsv.sh
@@ -1,9 +1,9 @@
 
-client_id='organization.17fac31c-f515-44cb-b88e-af4900f6d531'
-client_secret='WNZaEmgfcg11OlPrNIuWAYCeghA8Je'
+client_id='organization.xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
+client_secret='yourclientsecret'
 scope='api.organization'
 grant_type='client_credentials'
-vault_uri='https://bitwardengrafana.atjb.link'
+vault_uri='https://your.vault.uri'
 
   #!/bin/bash
 
@@ -13,8 +13,8 @@ echo $scope
 echo $grant_type
 echo $vault_uri
 
-ACCESS_TOKEN="$(curl -X POST $vault_uri/identity/connect/token -H 'Content-Type: application/x-www-form-urlencoded' -d 'grant_type=client_credentials&scope=api.organization&client_id=organization.17fac31c-f515-44cb-b88e-af4900f6d531&client_secret=WNZaEmgfcg11OlPrNIuWAYCeghA8Je' | jq -cr '.access_token')"
+ACCESS_TOKEN="$(curl -X POST $vault_uri/identity/connect/token -H 'Content-Type: application/x-www-form-urlencoded' -d 'grant_type=client_credentials&scope=api.organization&client_id=organization.xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx&client_secret=yourclientsecret' | jq -cr '.access_token')"
 
 echo $ACCESS_TOKEN
 
-curl -X GET $vault_uri/api/public/events/ -H 'Authorization: Bearer '$ACCESS_TOKEN'' | jq -r '.data[] | join (",")' > /home/adam/bwEventLogs/event_logs.csv
+curl -X GET $vault_uri/api/public/events/ -H 'Authorization: Bearer '$ACCESS_TOKEN'' | jq -r '.data[] | join (",")' > /downloads/bwEventLogs/event_logs.csv
