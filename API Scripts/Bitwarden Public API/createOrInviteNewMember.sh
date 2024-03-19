ca6ec344555efed84514ab01d5be6eba1b9c4572
Stacy Grimes <140400457+stacygrimes@users.noreply.github.com>
2024-03-19 15:29:19 -0400

    Update createOrInviteNewMember.sh

--- API Scripts/Bitwarden Public API/createOrInviteNewMember.sh
+++ API Scripts/Bitwarden Public API/createOrInviteNewMember.sh
@@ -1,1 +1,10 @@
+## Script downloads Bitwarden Vault Event Logs
+org_client_id='organization.xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
+org_client_secret='xxxxxxxxxxxxxxxxxxxxxx'
+scope='api.organization'
+grant_type='client_credentials'
+vault_uri='https://vault.bitwarden.com'
+
+#!/bin/bash
+
 curl -X POST https://api.bitwarden.com/public/members -H 'Authorization: Bearer <TOKEN>' -H "accept: application/json" -H "Content-Type: application/json" -d '{"email":"user1@example.com","collections":[{"id":"2d66c744-b186-421b-951c-acc3005cf548","readOnly":false}],"type":0,"accessAll":true,"externalId":"null"}'
