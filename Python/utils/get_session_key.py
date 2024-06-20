e677d2e34cd0f3abef290e40295db58694ba67c1
Adam Thomas Jagger Bramley <atjbramley@hotmail.com>
2024-06-20 09:26:23 +0200

    remove debug, return python object instead of setting global env var

--- Python/utils/get_session_key.py
+++ Python/utils/get_session_key.py
@@ -1,4 +1,3 @@
-import json
 import os
 import re
 import subprocess
@@ -6,32 +5,15 @@ from utils.decrypt_env import decrypt_secrets
 
 bw = '/Users/adambramley/.nvm/versions/node/v19.9.0/bin/bw'
 
-decrypt_secrets()
-
 def get_session_key():
+  decrypt_secrets()
   owner_username = os.environ['owner_username']
   owner_masterpassword = os.environ['owner_masterpassword']
 
-  # print(f"username is: {owner_username}")
-  # print(os.environ['owner_masterpassword'])
-
   logout = subprocess.run([bw, 'logout'])
 
-  login_command = [bw, 'login', owner_username, os.environ['owner_masterpassword']]
-  print(f"login command is {login_command}")
+  login_command = [bw, 'login', owner_username, owner_masterpassword]
 
   login = subprocess.run(login_command, capture_output=True, text=True)
-  session_key = re.search(r'(?=\").*(\")', login.stdout)
-  print(session_key)
-  os.environ['BW_SESSION'] = session_key
+  session_key = re.search(r'"[^"]+"', login.stdout).group(0)
   return session_key
-
-# print(str(match.group(0)))
-# print("STDOUT:", login.stdout)
-# print("STDERR:", login.stderr)
-
-# unlock_command = [bw, 'unlock', os.environ['owner_masterpassword']]
-# unlock = subprocess.run(unlock_command, capture_output=True, text=True)
-# session_key = json.loads(unlock.stdout)
-
-# print(session_key)
