0904fd20dd6fdf605e42f4ea25bb4a54a6cdb017
fer <fer@ferqwerty.com>
2024-08-07 17:24:13 +0200

    no output to adam's folder

--- API Scripts/Bitwarden Public API/downloadEventLogs.sh
+++ API Scripts/Bitwarden Public API/downloadEventLogs.sh
@@ -8,4 +8,4 @@ grant_type='client_credentials'
 vault_uri='https://vault.bitwarden.com'
 
 ACCESS_TOKEN="$(curl -X POST $vault_uri/identity/connect/token -H 'Content-Type: application/x-www-form-urlencoded' -d 'grant_type=client_credentials&scope=api.organization&client_id='$org_client_id'&client_secret='$org_client_secret'' | cut -d '"' -f4)"
-curl -X GET $vault_uri/api/public/events/ -H 'Authorization: Bearer '$ACCESS_TOKEN'' -H "Accept: application/json" -o '/home/adam/bwEventLogs/eventLogs.txt'
+curl -X GET $vault_uri/api/public/events/ -H 'Authorization: Bearer '$ACCESS_TOKEN'' -H "Accept: application/json" # -o '/home/adam/bwEventLogs/eventLogs.txt'
