02ea51e4a752f846aa1291f782a7035c42021d3f
Kristofer Tingdahl <10433010+tingdahl@users.noreply.github.com>
2024-08-28 13:54:14 +0200

    Optimized code for fewer API calls. Made it use emails instead of names, as names are not always set.

--- Bash Scripts/createCollectionsForAllMembersNested.sh
+++ Bash Scripts/createCollectionsForAllMembersNested.sh
@@ -20,34 +20,74 @@ else
 fi
 
 # Set up CLI and API auth
+org_client_secret_key=$(cat secureString_secret.txt \
+	| openssl enc -aes-256-cbc -md sha512 -a -d -pbkdf2 -iter 100000 \
+	 -salt -pass pass:Secret@Bitwarden#99)
 
-org_client_secret_key=$(cat secureString_secret.txt | openssl enc -aes-256-cbc -md sha512 -a -d -pbkdf2 -iter 100000 \
- -salt -pass pass:Secret@Bitwarden#99)
-
-password=$(cat secureString.txt | openssl enc -aes-256-cbc -md sha512 -a -d -pbkdf2 -iter 100000 \
- -salt -pass pass:Secret@Bitwarden#99)
+password=$(cat secureString.txt \
+	| openssl enc -aes-256-cbc -md sha512 -a -d -pbkdf2 -iter 100000 \
+	 -salt -pass pass:Secret@Bitwarden#99)
 
 session_key="$(printf $password | bw unlock --raw)"
 export BW_SESSION="$session_key"
-bearer_token="$(curl -sX POST $identity_url/connect/token -H 'Content-Type: application/x-www-form-urlencoded' -d 'grant_type=client_credentials&scope=api.organization&client_id='organization.$organization_id'&client_secret='$org_client_secret_key'' | cut -d '"' -f4)"
-org_members="$(curl -sX GET $api_url/public/members -H 'Content-Type: application/json' -H 'Authorization: Bearer '$bearer_token'' | jq -r '.data[] | select(.status == 2) | .name')"
 
-IFS=$'\n'
+bearer_token="$(curl -sX POST $identity_url/connect/token \
+	-H 'Content-Type: application/x-www-form-urlencoded' \
+	-d 'grant_type=client_credentials&scope=api.organization&client_id='organization.$organization_id'&client_secret='$org_client_secret_key'' \
+	| cut -d '"' -f4)"
 
+all_collections="$(bw list org-collections --organizationid $organization_id)"
+collection_template="$(bw get template org-collection)"
+
+allorgmembers="$(curl -sX GET $api_url/public/members \
+	-H 'Content-Type: application/json' \
+	-H 'Authorization: Bearer '$bearer_token'' \
+	| jq 'del(.object)')"
+
+org_members="$(jq -r '.data[] | select(.status == 2) | .email' <<< "$allorgmembers")"
+
+#Loop over all users and create a collection with their e-mail
 for member in ${org_members[@]}; do
 
-if (bw list org-collections --organizationid $organization_id | jq -r '.[].name' | grep -x "Personal Collections/$member" > /dev/null); then
-echo "$member already has a Collection, skipping"
+    if (echo $all_collections | jq -r '.[].name' | grep -x "Personal Collections/$member" > /dev/null); then
+    echo "$member already has a Collection, skipping"
 
 else
 
-allorgmembers="$(curl -sX GET $api_url/public/members -H 'Content-Type: application/json' -H 'Authorization: Bearer '$bearer_token'' | jq 'del(.object)')"
-memberid="$(jq -r -c --arg n "$member" '.data[] | select(.name==$n) | .id' <<< "$allorgmembers")"
-origmemberbody="$(curl -sX GET $api_url/public/members/$memberid -H 'Content-Type: application/json' -H 'Authorization: Bearer '$bearer_token''| jq -r -c 'del(.object)')"
-collectionid="$(bw get template org-collection | jq -c --arg n "$member" --arg c "$organization_id" '.name="Personal Collections/" + $n | .organizationId=$c | del(.groups[1])' | bw encode | bw create org-collection --organizationid $organization_id | jq -r '.id')"
-newmemberbody=$(jq -r -c --arg newcol "$collectionid" --arg i "$memberid" 'select(.id==$i) | .collections += [{"id": $newcol, "readOnly": false, "hidePasswords": false, "manage": true}]' <<< "$origmemberbody")
-curl -sX PUT $api_url/public/members/$memberid -H 'Content-Type: application/json' -H 'Authorization: Bearer '$bearer_token'' -d $newmemberbody > /dev/null
-echo "Created Collection for $member"
+    memberid="$(jq -r -c --arg n "$member" '.data[] | select(.email==$n) | .id' <<< "$allorgmembers")"
+
+    collectionid="$(echo $collection_template \
+	| jq -c --arg n "$member" --arg c "$organization_id" --arg m "$memberid" \
+	'.name="Personal Collections/" + $n | .organizationId=$c | del(.groups[1]) | del(.groups[0]) | del(.users[1]) | .users[0].id=$m | .users[0].manage=true' \
+	| bw encode \
+	| bw create org-collection --organizationid $organization_id \
+	| jq -r '.id')"
+
+    echo "Created Collection for $member"
+fi
+
+done
+
+
+
+
+for member in ${org_members[@]}; do
+
+    if (echo $all_collections | jq -r '.[].name' | grep -x "Personal Collections/$member" > /dev/null); then
+    echo "$member already has a Collection, skipping"
+
+else
+
+    memberid="$(jq -r -c --arg n "$member" '.data[] | select(.email==$n) | .id' <<< "$allorgmembers")"
+
+    collectionid="$(echo $collection_template \
+	| jq -c --arg n "$member" --arg c "$organization_id" --arg m "$memberid" \
+	'.name="Personal Collections/" + $n | .organizationId=$c | del(.groups[1]) | del(.groups[0]) | del(.users[1]) | .users[0].id= $m | .users[0].manage=true' \
+	| bw encode \
+	| bw create org-collection --organizationid $organization_id \
+	| jq -r '.id')"
+
+    echo "Created Collection for $member"
 fi
 
 done
