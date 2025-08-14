0a4f5f152a8af25cef339139b7f7481f1a217db6
amc950 <71721678+amc950@users.noreply.github.com>
2025-08-14 14:54:24 -0400

    changed powershell script to move users to admin instead of manager

---
+++ Powershell/bwConfirmAcceptedPeople_MoveToAdmin.ps1
@@ -0,0 +1,49 @@
+# Depends on file "secureString.txt" which can be created by first running:
+# Read-Host "Enter Master Password" -AsSecureString | ConvertFrom-SecureString | Out-File "secureString.txt"
+# Depends on file "secureString_secret.txt" which can be created by first running:
+# Read-Host "Enter client_secret" -AsSecureString | ConvertFrom-SecureString | Out-File "secureString_secret.txt"
+# jq is required in $PATH https://stedolan.github.io/jq/download/
+# bw is required in $PATH and logged in https://bitwarden.com/help/cli/
+
+$organization_id = "YOUR-ORG-ID" # Set your Org ID
+$cloud_flag = 1 # Self-hosted Bitwarden or Cloud?
+if ($cloud_flag -eq 1) {
+    $api_url = "https://api.bitwarden.com"
+    $identity_url = "https://identity.bitwarden.com"
+}
+else {
+    $api_url = "https://YOUR-FQDN/api" # Set your Self-Hosted API URL
+    $identity_url = "https://YOUR-FQDN/identity" # Set your Self-Hosted Identity URL
+}
+
+# Set up CLI and API auth
+
+$org_client_secret = Get-Content "secureString_secret.txt" | ConvertTo-SecureString
+$client_creds = New-Object System.Management.Automation.PSCredential "null", $org_client_secret
+$org_client_secret_key =  , $client_creds.GetNetworkCredential().password
+$org_client_id = "organization." + $organization_id
+
+# Get Access Token
+
+$body = "grant_type=client_credentials&scope=api.organization&client_id=$org_client_id&client_secret=$org_client_secret_key"
+$bearer_token = (Invoke-RestMethod -Method POST -Uri $identity_url/connect/token -Body $body).access_token
+
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add('Authorization',('Bearer {0}' -f $bearer_token))
+$headers.Add('Accept','application/json')
+$headers.Add('Content-Type','application/json')
+
+# Perform CLI and API auth
+
+$password = Get-Content "secureString.txt" | ConvertTo-SecureString
+$cred = New-Object System.Management.Automation.PSCredential "null", $password
+$session_key = , $cred.GetNetworkCredential().password | powershell -c 'bw unlock --raw'
+$org_members = bw list --session $session_key org-members --organizationid $organization_id | jq -c '.[] | select(.status == 1)' | jq -c '.id'
+
+ForEach ($member_id in $org_members.trim('"')) {
+    bw confirm --session $session_key org-member $member_id --organizationid $organization_id
+    Write-Host "Upgrading to admin: $member_id"
+    $member_data = (Invoke-RestMethod -Method GET -Uri $api_url/public/members/$member_id -Headers $headers)
+    $params = @{type=2;accessAll=$member_data.accessAll;externalId=$member_data.externalId;resetPasswordEnrolled=$member_data.resetPasswordEnrolled;collections=$member_data.collections} | ConvertTo-Json
+    Invoke-RestMethod -Method PUT -Uri $api_url/public/members/$member_id -Headers $headers -Body $params
+}
